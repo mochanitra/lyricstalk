6f655d9cfc2b87be015e9278e0c73ea7ef0074e6
Por <nattida@5lab.co>
2018-10-12 16:56:43 +0700

    - update

---
+++ components/boxes/PartnerBox.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="_mgbt-48px">
+    <div class="_dp-f _mgbt-24px">
+      <div class="_ttf-upc _fs-8 _pdr-2px partner-heading">{{ heading }}</div>
+      <div 
+        v-if="hasLine" 
+        class="_h-1px _bgcl-blue _w-at"/>
+    </div>
+    <div class="_dp-f _w-100pct">
+      <div 
+        v-for="(p,i) in partners" 
+        :key="i" 
+      >
+        <img 
+          v-lazy="p.image" 
+          class="_w-100pct _h-at _pdr-32px">
+          <!-- <div 
+          v-lazy:background="p.image" 
+          class="_bgs-ct _bgrp-nrp _w-128px _h-64px" /> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    heading: {
+      default: 'Title',
+      type: String
+    },
+    partners: {
+      default: () => [],
+      type: Array
+    },
+    hasLine: {
+      default: true,
+      type: Boolean
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.partner-heading {
+  position: relative;
+  width: 100%;
+
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    border: 0;
+    height: 1px;
+    background-color: red;
+    z-index: -1;
+  }
+}
+</style>
+
+
+
+
--- components/sections/Partners.vue
+++ components/sections/Partners.vue
@@ -1,14 +1,36 @@
 <template>
   <div class="row">
     <div class="col-12 col-md-8">
-      <div class=""/>
+      <PartnerBox 
+        :heading="`Organised by`" 
+        :partners="orginised"/>
     </div>
-    <div class="col-12 col-md-4"/>
+    <div class="col-12 col-md-4">
+      <PartnerBox 
+        :heading="`Offial Partners`" 
+        :partners="officialPartners"/>
+    </div>
+    <div class="col-12 col-md-3">
+      <PartnerBox 
+        :heading="`Top Supporter`" 
+        :partners="topSupporter"
+        :has-line="false"
+      />
+    </div>
+    <div class="col-12 col-md-9">
+      <PartnerBox 
+        :heading="`Major Supporters`" 
+        :partners="majorSupporters"/>
+    </div>
   </div>
 </template>
 
 <script>
+import PartnerBox from '~/components/boxes/PartnerBox'
 export default {
+  components: {
+    PartnerBox
+  },
   data: () => ({
     orginised: [
       {
@@ -21,7 +43,32 @@ export default {
         image: 'http://via.placeholder.com/100x60'
       }
     ],
-    official: [
+    officialPartners: [
+      {
+        image: 'http://via.placeholder.com/100x60'
+      },
+      {
+        image: 'http://via.placeholder.com/100x60'
+      },
+      {
+        image: 'http://via.placeholder.com/100x60'
+      }
+    ],
+    topSupporter: [
+      {
+        image: 'http://via.placeholder.com/100x60'
+      }
+    ],
+    majorSupporters: [
+      {
+        image: 'http://via.placeholder.com/100x60'
+      },
+      {
+        image: 'http://via.placeholder.com/100x60'
+      },
+      {
+        image: 'http://via.placeholder.com/100x60'
+      },
       {
         image: 'http://via.placeholder.com/100x60'
       },
