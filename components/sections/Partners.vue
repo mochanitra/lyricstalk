1603811caede6d7da80db3cb447d3791ddad5889
Por <nattida@5lab.co>
2018-10-12 15:50:06 +0700

    - update

---
+++ components/sections/Partners.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="row">
+    <div class="col-12 col-md-8">
+      <div class=""/>
+    </div>
+    <div class="col-12 col-md-4"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    orginised: [
+      {
+        image: 'http://via.placeholder.com/100x60'
+      },
+      {
+        image: 'http://via.placeholder.com/100x60'
+      },
+      {
+        image: 'http://via.placeholder.com/100x60'
+      }
+    ],
+    official: [
+      {
+        image: 'http://via.placeholder.com/100x60'
+      },
+      {
+        image: 'http://via.placeholder.com/100x60'
+      },
+      {
+        image: 'http://via.placeholder.com/100x60'
+      }
+    ]
+  })
+}
+</script>
+
+
+
+
+
+
