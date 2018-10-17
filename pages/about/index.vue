5b58861d6a0a5edebd1a0742395828a3dd00a12c
Apon Palanuwech <palaa159@newschool.edu>
2018-10-17 09:18:03 +0700

    - update

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -45,38 +45,6 @@ export default {
     Partners,
     ImageBoxWithTitle
   },
-  data: () => ({ 
-    about: {
-      image: 'https://placeimg.com/640/480/any',
-      content: `
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-      <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
-      `
-    },
-
-    aboutImages: [
-      {
-        image: 'https://placeimg.com/640/480/any',
-        title: 'Factsheet',
-        path: '/'
-      },
-      {
-        image: 'https://placeimg.com/640/480/any',
-        title: 'Overview',
-        path: '/'
-      },
-      {
-        image: 'https://placeimg.com/640/480/any',
-        title: 'Photo Library',
-        path: '/'
-      },
-      {
-        image: 'https://placeimg.com/640/480/any',
-        title: 'Video Library',
-        path: '/'
-      }
-    ]
-  })
 }
 </script>
 
